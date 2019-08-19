2ee5fb5768d602844b96a19bf101c644dfe3028a
Anastasiya Kostina <anastasiya.kostina@rubius.com>
2019-08-19 18:33:16 +0700

    Change layout

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -5,7 +5,6 @@ import { Store } from '@ngrx/store';
 import * as fromBooks from '../store';
 import * as fromBooksAction from '../store/books.actions';
 
-import { FirebaseService } from '../services/firebase.service';
 import { IBook } from '../models/book.model';
 
 @Component({
@@ -24,15 +23,12 @@ export class BooksComponent implements OnInit {
   public books: IBook[] = [];
 
   constructor(
-    private firebaseService: FirebaseService,
     private store: Store<IBook>
   ) {
     this._searchSubject = new Subject<string>();
   }
 
   ngOnInit() {
-    this.firebaseService.getBooks();
-    this.store.select(fromBooks.selectAllBooks).subscribe(books => this.books =  books);
     this._searchSubject
       .pipe(
         debounceTime(300),
