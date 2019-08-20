b07523d0201b008eaf2e5a1f5b121264debfe463
Anastasiya Kostina <anastasiya.kostina@rubius.com>
2019-08-20 22:10:21 +0700

    Add  web worker for filtering in background

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, Observable, of } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import * as fromBooks from '../store';
@@ -21,21 +21,25 @@ export class BooksComponent implements OnInit, OnDestroy {
   public searchStringControl: FormControl;
 
   public books$: Observable<Book[]>;
+  private _books: Book[];
   private _filterSubscription: Subscription;
   private _filterStringSubscription: Subscription;
 
+  private _filterWebWorker: Worker;
   constructor(
     private store: Store<Book>
   ) {
     this.searchStringControl = new FormControl();
-    this.books$ = this.store.select(fromBooks.getFilteredBooks);
+    this.books$ = new Observable<Book[]>();
   }
 
   ngOnInit() {
+    this.store.select(fromBooks.selectAllBooks).subscribe(b => this._books = b);
     this._filterStringSubscription = this.store.select(fromBooks.selectSearchString)
       .subscribe(str => {
         if (!this.searchStringControl.value) {
           this.searchStringControl.setValue(str);
+          this.startWebWorker(str);
         }
       });
     this._filterSubscription = this.searchStringControl
@@ -47,6 +51,7 @@ export class BooksComponent implements OnInit, OnDestroy {
         this.store.dispatch(fromBooksAction.setSearchString({
           searchString: data
         }));
+        this.startWebWorker(data);
       });
   }
 
@@ -55,6 +60,21 @@ export class BooksComponent implements OnInit, OnDestroy {
       this._filterSubscription.unsubscribe();
     if (this._filterStringSubscription)
       this._filterStringSubscription.unsubscribe();
-
   }  
+
+  startWebWorker(searchInput: string) {
+    if (typeof Worker !== 'undefined') {
+      if (!this._filterWebWorker) {
+        this._filterWebWorker = new Worker('../web-worker/filter-worker.worker', { type: 'module' });
+      }
+      this._filterWebWorker.onmessage = ({ data }) => {
+        this.books$ = of(data);
+      };
+      const data = {
+        books: this._books, 
+        searchString : searchInput
+      }
+      this._filterWebWorker.postMessage(data);
+    }
+  }
 }
---
+++ src/app/web-worker/filter-worker.worker.ts
@@ -0,0 +1,31 @@
+import { Book } from "../models/book.model";
+
+addEventListener('message', ({ data }) => {
+  let { books, searchString } = data;
+  const resultBooks: Book[] = [];
+  if (searchString) {
+    const lowSearchString = searchString.toLowerCase();
+    for (const value of books) {
+      const temp_book = value;
+      for (const property in value) {
+        if (value.hasOwnProperty(property)) {
+          const element = value[property];
+          if (!(element instanceof Array) && element.toString().toLowerCase().indexOf(lowSearchString) > -1) {
+            temp_book.FieldToShow = property;
+            temp_book.ValueToShow = element;
+            resultBooks.push(temp_book);
+            break;
+          } else if (element instanceof Array) {
+            if (element.some(author => author.toLowerCase().indexOf(lowSearchString) > -1)) {
+              temp_book.FieldToShow = property;
+              temp_book.ValueToShow = element.join(', ');
+              resultBooks.push(temp_book);
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+  postMessage(resultBooks);
+});
